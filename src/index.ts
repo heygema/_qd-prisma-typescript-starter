796b7e218be571a504a181a9fe155fe0a17b2986
Gema <gem.anggada@gmail.com>
2021-01-17 21:11:11 +0700

    with socket.io

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import path from "path";
 import express from "express";
 import http from "http";
 import { ApolloServer, gql } from "apollo-server-express";
+import makeIO from "socket.io";
 
 const typeDefs = gql`
   type Query {
@@ -32,8 +33,14 @@ app.use(express.static(path.join(__dirname, "../public")));
 apolloServer.applyMiddleware({ app });
 let server = http.createServer(app);
 
+// @ts-ignore
+const io = makeIO(server);
+
+io.on("connection", (socket: any) => {
+  console.log('joined');
+  socket.emit("message", `Welcome ${socket.id}`);
+});
 
 server.listen(4000, () => {
-  console.log(`Server running on http://localhost${4000}`);
   console.log(`Server running on http://localhost:${4000}`);
 });
